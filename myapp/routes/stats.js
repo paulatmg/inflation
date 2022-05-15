eece1a0c60dd7a530c735806a7d4dc4348b05923
Macbook Pro <macbookpro@MacBook-Pro-de-Macbook.local>
2022-05-15 13:11:07 -0300

    Final2

--- myapp/routes/stats.js
+++ myapp/routes/stats.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var functions = require('../myFunctions/functions2');
+var functions = require('../myFunctions/functions');
 var moment = require('moment');
 const axios = require('axios').default;
 
